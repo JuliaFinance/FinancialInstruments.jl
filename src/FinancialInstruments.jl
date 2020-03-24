a8960c26695929b980056a392e2d5bd98b98c29e
Eric Forgy <Eric.Forgy@coherentasia.com>
2020-03-24 01:30:54 -0700

    Add list

--- src/FinancialInstruments.jl
+++ src/FinancialInstruments.jl
@@ -6,11 +6,20 @@ export FinancialInstrument, Cash, Currencies
 
 abstract type FinancialInstrument end
 
-struct Cash{C<:Currency} <: FinancialInstrument end
+struct Cash{C<:Currency} <: FinancialInstrument
+    function Cash{T}() where T
+        c = new()
+        list[Currencies.symbol(T)] = c
+        return c
+    end
+end
 Cash(c::C) where C<:Currency = Cash{C}()
+const list = Dict{Symbol,FinancialInstrument}()
 
 for (sym,ccy) in Currencies.list
-    @eval FinancialInstruments $sym = Cash($ccy)
+    @eval FinancialInstruments begin
+        $sym = Cash($ccy)
+    end
 end
 
 end # module
